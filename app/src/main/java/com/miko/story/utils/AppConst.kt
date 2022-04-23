a9067675294017ac389792ad34a927c47dac1fd6
khukuh123 <khukuh.prihatmikho2@gmail.com>
2022-04-23 23:40:43 +0700

    fix(story): fix the code based on review feedback

--- app/src/main/java/com/miko/story/utils/AppConst.kt
+++ app/src/main/java/com/miko/story/utils/AppConst.kt
@@ -4,6 +4,4 @@ import com.miko.story.BuildConfig
 
 object AppConst {
     const val API_URL_BASE = BuildConfig.API_BASE_URL
-//    const val GITHUB_API_KEY = BuildConfig.GITHUB_API_KEY
-//    const val GITHUB_URL_BASE = "https://github.com/"
 }
